da13f8ddee7b48c1ae769d8dfb34e6c3ad6a1dc7
Calos <carlosc7502@gmail.com>
2022-07-31 00:07:05 -0400

    alien_invasion P11

--- alien_invation/settings.py
+++ alien_invation/settings.py
@@ -13,7 +13,7 @@ class Settings:
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
@@ -23,6 +23,9 @@ class Settings:
 
         # How quickly the game speeds up
         self.speedup_scale = 1.05
+
+        # How quicly the alien points increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -37,12 +40,17 @@ class Settings:
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
 
+        # Scoring
+        self.alien_points = 50
+
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien points value."""
 
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
+
 
         # if self.bullet_width < 300:
         #     self.bullet_width *= 10
